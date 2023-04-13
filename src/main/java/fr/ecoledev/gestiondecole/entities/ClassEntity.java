a9ace4cdb488d3966420d25669b96a4b8e1fd6dd
RositaElow <elowrosita99@gmail.com>
2023-04-13 17:07:53 +0200

    app: modified table names

--- src/main/java/fr/ecoledev/gestiondecole/entities/ClassEntity.java
+++ src/main/java/fr/ecoledev/gestiondecole/entities/ClassEntity.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name = "classes")
 public class ClassEntity {
    
 	@Id 
